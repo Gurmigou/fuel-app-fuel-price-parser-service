f85a2b0a8543f9fd6c8e3c628e1d880aeb9378f4
Gurmigou <yehor.bukhanets@gmail.com>
2022-02-05 16:32:23 +0200

    discovery client added

--- src/main/java/com/fueladvisor/fuelpriceparserservice/FuelPriceParserServiceApplication.java
+++ src/main/java/com/fueladvisor/fuelpriceparserservice/FuelPriceParserServiceApplication.java
@@ -2,8 +2,10 @@ package com.fueladvisor.fuelpriceparserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FuelPriceParserServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/fueladvisor/fuelpriceparserservice/controller/FuelInfoController.java
+++ src/main/java/com/fueladvisor/fuelpriceparserservice/controller/FuelInfoController.java
@@ -4,10 +4,7 @@ import com.fueladvisor.fuelpriceparserservice.model.dto.FuelInfoDto;
 import com.fueladvisor.fuelpriceparserservice.service.FuelInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,10 +18,10 @@ public class FuelInfoController {
         this.fuelInfoService = fuelInfoService;
     }
 
-//    @PostMapping("/update")
-//    public void updateData() {
-//        fuelInfoService.updateFuelData();
-//    }
+    @PostMapping("/update")
+    public void updateData() {
+        fuelInfoService.updateFuelData();
+    }
 
     @GetMapping
     public ResponseEntity<List<FuelInfoDto>> getFuelInfoInRegion(@RequestParam String region) {
